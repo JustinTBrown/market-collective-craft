9c6085552fe4c493ed40bd6200c3bc7a3dce5300
JustinBrown <jb@justinbrown.io>
2017-04-19 14:19:22 -0600

    Config update.

--- craft/config/general.php
+++ craft/config/general.php
@@ -29,9 +29,7 @@ return array(
   ),
 
   // Development - Digital Ocean
-  // test
   '162.243.199.96' => array(
-  // 'mc.generalweb.co' => array(
     'devMode' => true,
     'environmentVariables' => array(
       'siteUrl'  => '/',
