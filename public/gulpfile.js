3171f0c8e1eea678f5dd650260853cd144f718d0
JustinBrown <jb@justinbrown.io>
2017-04-18 19:15:23 -0600

    Gulp clean up. Style guide.

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -12,36 +12,13 @@ gulp.task('sass', function () {
     .pipe(livereload());
 });
 
-// gulp.task('reload', function () {
-//   livereload();
-// });
-
-gulp.task('sass:watch', function () {
+gulp.task('watch', function () {
   livereload.listen();
   gulp.watch([
     './scss/**/*.scss',
     'gulpfile.js',
     './node_modules/foundation-sites/scss/**/*.scss',
   ], ['sass']);
-  // gulp.watch([
-  //   '../craft/templates/**/*.html',
-  // ], ['reload']);
 });
 
-
-// gulp.task('templates', function () {
-//   livereload.listen();
-//   gulp.watch([
-//     '../craft/templates/**/*.html',
-//   ]);
-// });
-
-// gulp.task('html', function() {
-//   gulp.src('./craft/templates/**/*.html')
-//     .pipe(changed('.'))
-//     .pipe(livereload());
-// });
-
-gulp.task('default', [ 'sass', 'sass:watch', ]);
-
-// gulp.task('production-build', [ 'html', 'css' ]);
+gulp.task('default', [ 'sass', 'watch', ]);
